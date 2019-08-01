56a19908de0d6541392f50a4a7f4884fa9bca710
kk <zikerwang@gmail.com>
2019-08-01 17:08:35 +0800

    decoupled the func 'Hello' to two functions

--- Hello-World/hello.go
+++ Hello-World/hello.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello, "
+const englishPrefix = "Hello, "
 const spanish = "Spanish"
 const spanishHelloPrefix = "Hola, "
 const french = "French"
@@ -13,16 +13,21 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := helloPrefix
+	return choosePrefix(language) + name
+}
 
-	switch language {
+func choosePrefix(language string) (prefix string){
+	switch language{
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishPrefix
 	}
-
-	return prefix + name
+	return
+	// this is different from C, we can just call 'return' rather than 'return prefix'
+	// to returns the return value of the function we defined (i.e. prefix)
 }
 
 func main() {
