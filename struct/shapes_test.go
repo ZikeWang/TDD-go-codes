164a75203a675160ba6906302062dff30acbc0ce
kk <zikerwang@gmail.com>
2019-08-11 21:20:25 +0800

    refactored to add assertion of truth about shapes and their areas

--- struct/shapes_test.go
+++ struct/shapes_test.go
@@ -1,4 +1,4 @@
-package perimeter
+package main
 
 import "testing"
 
@@ -14,18 +14,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTest := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{10.0, 8.0}, 80.0},
-		{Circle{10.0}, 314.1592653589793},
-		{Triangle{6.0, 6.0}, 18.0},
+		{name: "Rectangle", shape: Rectangle{width: 10.0, height: 8.0}, want: 80.0},
+		{name: "Circle", shape: Circle{radius: 10.0}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{base: 6.0, height: 6.0}, want: 18.0},
 	}
 
 	for _, testcase := range areaTest {
-		got := testcase.shape.Area()
-		if got != testcase.want {
-			t.Errorf("got %f want %f", got, testcase.want)
-		}
+		t.Run(testcase.name, func(t *testing.T) {
+			got := testcase.shape.Area()
+			if got != testcase.want {
+				t.Errorf("%#v, got %f want %f", testcase.shape, got, testcase.want)
+			}
+		})
 	}
 }
