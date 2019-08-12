86ecb5c9e87e853dc8c1d0dba6306a2ec83c06fa
kk <zikerwang@gmail.com>
2019-08-12 21:51:23 +0800

    add Add function and tests

--- map/dictionary.go
+++ map/dictionary.go
@@ -1,12 +1,27 @@
 package main
 
-import "errors"
-
-//Dictionary is a map type I defined
+//Dictionary is a map type
 type Dictionary map[string]string
 
-//ErrNotFound is an error message I defined as a variant
-var ErrNotFound = errors.New("couldn't find the word you search")
+//DictionaryErr is a string type
+type DictionaryErr string
+
+/*
+var (
+	ErrNotFound  = errors.New("couldn't find the word you search")
+	ErrKeyExists = errors.New("found existing key you want to add")
+)
+*/
+
+//ErrNotFound and ErrKeyExists are defined error message
+const (
+	ErrNotFound  = DictionaryErr("couldn't find the word you search")
+	ErrKeyExists = DictionaryErr("found existing key you want to add")
+)
+
+func (d DictionaryErr) Error() string {
+	return string(d) //this method's return type is string rather than self-defined type DictionaryErr, so we need an explicit transformation
+}
 
 //Search is a function return map search results, including value and error message
 func (d Dictionary) Search(key string) (string, error) {
@@ -15,4 +30,18 @@ func (d Dictionary) Search(key string) (string, error) {
 		return "", ErrNotFound
 	}
 	return value, nil
+}
+
+//Add can add key and value into map
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+	switch err {
+	case ErrNotFound:
+		d[key] = value
+	case nil:
+		return ErrKeyExists
+	default:
+		return err
+	}
+	return nil
 }
--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -2,13 +2,6 @@ package main
 
 import "testing"
 
-func assertError(t *testing.T, got, want error) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got error '%s' want '%s'", got, want)
-	}
-}
-
 func TestMapSearch(t *testing.T) {
 	t.Run("known word", func(t *testing.T) {
 		dictionary := Dictionary{"Test": "It is just a test"}
@@ -27,4 +20,44 @@ func TestMapSearch(t *testing.T) {
 		}
 		assertError(t, err, ErrNotFound)
 	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		key := "testAdd"
+		value := "this is an Add function test"
+		err := dictionary.Add(key, value)
+		assertError(t, err, nil)
+		assertValue(t, dictionary, key, value)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		key := "testAdd"
+		value := "this is an Add function test"
+		dictionary := Dictionary{key: value}
+		err := dictionary.Add(key, "new value")
+		assertError(t, err, ErrKeyExists)
+		assertValue(t, dictionary, key, value)
+	})
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error '%s' want '%s'", got, want)
+	}
+}
+
+func assertValue(t *testing.T, dictionary Dictionary, key, value string) {
+	t.Helper()
+	got, err := dictionary.Search(key)
+
+	if err == ErrNotFound {
+		t.Errorf("add failed")
+	}
+
+	if got != value {
+		t.Errorf("got '%s' want '%s'", got, value)
+	}
 }
