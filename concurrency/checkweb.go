76e530ab03fc700c0b74dceb77994e0378332b9c
kk <zikerwang@gmail.com>
2019-08-15 19:53:25 +0800

    add goroutine and channel ds, add some comments

--- concurrency/checkweb.go
+++ concurrency/checkweb.go
@@ -1,12 +1,35 @@
 package checkweb
 
+//WebChecker is a function type
 type WebChecker func(string) bool
 
+type result struct {
+	url string
+	tf  bool
+}
+
+/*
+type result struct {
+	string
+	bool
+}
+*/
+
+//CheckWebsite write judgement (by MockWebchecker) to map
 func CheckWebsite(wc WebChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.url] = result.tf
+		//results[result.string] = result.bool
 	}
 
 	return results
