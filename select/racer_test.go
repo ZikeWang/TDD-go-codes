bc00596fb601150dbfa194057ec9c20f789491ae
kk <zikerwang@gmail.com>
2019-08-16 16:22:29 +0800

    replace real reaching with mock Http server

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestRacer(t *testing.T) {
-	fastURL := "http://www.baidu.com"
-	slowURL := "http://www.github.com"
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2 * time.Second)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastURL := fastServer.URL
+	slowURL := slowServer.URL
 
 	want := fastURL
 	got := Racer(fastURL, slowURL)
@@ -14,4 +26,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
 	}
+
+	fastServer.Close()
+	slowServer.Close()
 }
