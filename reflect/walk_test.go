d89cc73b4a79a399ca01e69922611cca68e80d61
kk <zikerwang@gmail.com>
2019-08-20 23:59:53 +0800

    add table based test for string field test

---
+++ reflect/walk_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name     string
+		Input    interface{}
+		Expected []string
+	}{
+		{
+			Name: "struct test case with only one string field",
+			Input: struct {
+				Name string
+			}{"Chris"},
+			Expected: []string{"Chris"},
+		},
+
+		{
+			Name: "struct test case with two string fields",
+			Input: struct {
+				Name1 string
+				Name2 string
+			}{Name1: "Jack", Name2: "Mike"},
+			Expected: []string{"Jack", "Mike"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.Expected) {
+				t.Errorf("got %v want %v", got, test.Expected)
+			}
+		})
+	}
+}
