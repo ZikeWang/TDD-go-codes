e16bcb5d1c094f4dac6220fc63df7519105264ac
kk <zikerwang@gmail.com>
2019-08-21 09:27:38 +0800

    add comments

--- reflect/walk.go
+++ reflect/walk.go
@@ -5,6 +5,11 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 
+	/*
+	 * NumField() can not be used on a pointer Value
+	 * we need to extract the underlying value before
+	 * we can do that by using Elem()
+	 */
 	if val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
