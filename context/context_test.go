9fe0cf15e8c283d069172dea8c75aade13372bff
kk <zikerwang@gmail.com>
2019-08-25 11:32:05 +0800

    context with comments and  explained

---
+++ context/context_test.go
@@ -0,0 +1,62 @@
+package context3
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestServer(t *testing.T) {
+	data := "123456789"
+
+	t.Run("returns data from store", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		// Server()函数返回一个可用作HTTP handler的HandlerFunc类型自定义普通函数
+		svr := Server(store)
+
+		// NewRequest方法返回一个新的可传入服务器的请求，可用于传入到一个http.Handler进行测试
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		// NewRecorder方法返回一个初始化的ResponseRecorder
+		response := httptest.NewRecorder()
+
+		// ServeHTTP方法的作用是进行函数调用：svr(response, request)。具体过程即：调用store.Fetch(r.Context())和执行Fprint写操作
+		svr.ServeHTTP(response, request)
+
+		// 判断response是否写入正确
+		if response.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		/*
+		 * WithCancel方法接受一个Context并返回一个Context（即第一个返回值），返回的Context里面包含一个Done() channel
+		 * 当调用返回的cancel函数（即第二个返回值）或父Context的Done channel被关闭时，返回的Context（第一个返回值）的Done channel被关闭
+		 */
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		// AfterFunc方法会等待其第一个参数定义的时间流逝后，调用第二个参数传入的函数，此时即调用前面所述的cancel函数
+		time.AfterFunc(5*time.Millisecond, cancel)
+		// WithContext方法会返回调用者（即等号右侧request）的一个浅拷贝，该拷贝中包含了实参发生的context修改
+		request = request.WithContext(cancellingCtx)
+
+		// httptest.NewRecorder的返回值是 *ResponseRecorder， 同理自定义的SpyResponseWriter赋给response的初始化值也应是指针类型
+		response := &SpyResponseWriter{}
+
+		/*
+		 * 这里就是通过自定义的SpyResponseWriter实现响应结果为将written域写为true
+		 * 测试的期望结果是cancel执行成功，即不会写任何的response，因此written期望值应为false
+		 */
+		svr.ServeHTTP(response, request)
+
+		if response.written {
+			t.Error("a response should not have been written")
+		}
+	})
+}
---
+++ context/testdouble.go
@@ -0,0 +1,97 @@
+package context3
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+)
+
+// SpyStore allows you to simulate a store and see how its used
+type SpyStore struct {
+	response string
+	t        *testing.T
+}
+
+// Fetch returns response after a short delay
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+
+	//time.Sleep(8 * time.Millisecond)
+	go func() {
+		var result string
+		for _, c := range s.response {
+
+			/*
+			 * select中的case语句必须是一个channel操作
+			 * 1. 如果有多个case都可以运行，select会随机公平地选出一个执行，其他不会执行。
+			 * 2. 如果没有可运行的case语句，且有default语句，那么就会执行default的动作。
+			 * 3. 如果没有可运行的case语句，且没有default语句，select将阻塞，直到某个case通信可以运行
+			 */
+
+			select {
+
+			/*
+			 * Done在工作结束时返回一个关闭的channel，这代表这个context应该被取消
+			 * 如果context永远不会被取消则Done返回nil
+			 * 当cancel函数被调用时，WithCancel会安排Done关闭
+			 */
+
+			case <-ctx.Done():
+				s.t.Log("spy store got cancelled") //只有加上-v标志才会显示以下Log的打印内容
+				fmt.Println("cancelling")
+				return
+			default:
+				time.Sleep(6 * time.Millisecond)
+				result += string(c)
+				fmt.Println(string(c))
+			}
+		}
+		data <- result
+	}()
+
+	select {
+	//对Done的连续调用会返回相同的值
+	case <-ctx.Done():
+		fmt.Println("Done") //这里很奇怪的一点是加上8ms延迟后，（5ms，10ms）Done每次都在cancelling前面打印？？？？？？？？
+		return "", ctx.Err()
+	case res := <-data:
+		fmt.Println("Response")
+		return res, nil
+	}
+}
+
+/*
+ * 以下代码自定义了SpyResponseWriter结构体，并定义了满足 ResponseWriter 接口类型的三个方法。 ResponseWriter类型的定义如下：
+ * type ResponseWriter interface {
+ *     Header() Header
+ *     Write([]byte) (int, error)
+ *     WriteHeader(statusCode int)
+ * }
+ * 自定义的类型中所有方法都是结构体的written标志写为true，用于模拟完成了response的写入操作
+ * 定义这样一个结构体及其方法的目的就是测试在cancel的情况下不会完成写入response的操作，httptest.ResponseRecorder不支持这样的测试
+ */
+
+// SpyResponseWriter checks whether a response has been written
+type SpyResponseWriter struct {
+	written bool
+}
+
+// Header will mark written to true
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+// Write will mark written to true
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+// WriteHeader will mark written to true
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
+}
